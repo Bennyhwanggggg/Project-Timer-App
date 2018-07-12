db4c7497ba7cf65503b804d355e3ae35cee4618f
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2018-07-13 00:37:41 +1000

    about dialog added

--- app/src/main/java/benny/dev/tasktimer/MainActivity.java
+++ app/src/main/java/benny/dev/tasktimer/MainActivity.java
@@ -1,5 +1,7 @@
 package benny.dev.tasktimer;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements CursorRecyclerViewAdapter.OnTaskClickListener,
         AddEditActivityFragment.OnSaveClicked,
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements CursorRecyclerVie
     private boolean mTwoPane = false;
 
     public static final int DIALOG_ID_CANCEL_EDIT = 2;
+
+    // show dialog box
+    private AlertDialog mDialog = null; // module scope because we need to dismiss it in onStop e.g when orientation changes to avoid memory leak.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -137,12 +144,30 @@ public class MainActivity extends AppCompatActivity implements CursorRecyclerVie
             case R.id.menumain_settings:
                 break;
             case R.id.menumain_showAbout:
+                showAboutDialog();
                 break;
             case R.id.menumain_generate:
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void showAboutDialog(){
+        @SuppressLint("InflateParams") View messageView = getLayoutInflater().inflate(R.layout.about, null, false); // no root view we can sensibly use
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(messageView);
+
+        mDialog = builder.create();
+        mDialog.setCanceledOnTouchOutside(true);
+        builder.setTitle(R.string.app_name);
+        builder.setIcon(R.mipmap.ic_launcher);
+
+        TextView tv = (TextView) messageView.findViewById(R.id.about_version);
+        tv.setText("v"+BuildConfig.VERSION_NAME);
+
+        mDialog.show();
     }
 
     @Override
